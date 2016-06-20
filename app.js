7792c0c0b67e6f147ea314e7f41c0177b4939528
Dylan Holmes <dylanbholmes@gmail.com>
2016-06-20 16:45:21 -0700

    complete injections of unordered lists for all five locations

--- app.js
+++ app.js
@@ -191,3 +191,133 @@ listItem.textContent = 'Total: ' + Math.round(seaCenter.totalSales) + ' cookies'
 seaCenterID.appendChild(listItem);
 
 console.log(seaCenter.totalSales);
+
+// // //
+// // // //
+// // // //
+// // //
+
+// Capitol Hill Cookies Projection
+var capHill = {
+  minCustCapHill: 20,
+  maxCustCapHill: 38,
+  avgCookiesPerCustCapHill: 2.3,
+  hourlyCustomerCapHill: [],
+  hourlySalesCapHill: []
+};
+// method to generate random number of customers per hour and store number of customers per hour in hourlyCustomerCapHill
+var randomCustCapHill = function() {
+  return (Math.floor(Math.random() * (capHill.maxCustCapHill - capHill.minCustCapHill + 1)) + capHill.minCustCapHill);
+};
+
+var custThatHourCapHill = function() {
+  for(var i = 0; i < hours.length; i++) {
+    capHill.hourlyCustomerCapHill[i] = randomCustCapHill();
+  }
+  console.log(capHill.hourlyCustomerCapHill);
+};
+
+custThatHourCapHill();
+
+// Another option to fill hourlyCustomerCapHill is by using push
+    //capHill.hourlyCustomerCapHill.push(randomCustCapHill());
+
+// Calculate and store number of cookies purchased for a single hour and storer number of cookies sold per hour in hourlySalesCapHill
+var cookiesSoldThatHourCapHill = function () {
+  for(var i = 0; i < hours.length; i++) {
+    capHill.hourlySalesCapHill.push(capHill.avgCookiesPerCustCapHill * capHill.hourlyCustomerCapHill[i]);
+  }
+  console.log(capHill.hourlySalesCapHill);
+};
+
+cookiesSoldThatHourCapHill();
+
+// Display the values of each Pike array as unordered lists in the browser
+capHill.listSalesCapHill = function() {
+  var capHillID = document.getElementById('capHillID');
+  for (var i = 0; i < hours.length; i++) {
+    console.log(capHill.hourlySalesCapHill[i]);
+    var listItem = document.createElement('li');
+    listItem.textContent = hours[i] + ': ' + Math.round(capHill.hourlySalesCapHill[i]) + ' cookies';
+    capHillID.appendChild(listItem);
+  }
+};
+
+capHill.listSalesCapHill();
+
+capHill.totalSales = 0;
+
+for (var i = 0; i < hours.length; i++) {
+  capHill.totalSales += capHill.hourlySalesCapHill[i];
+};
+
+var listItem = document.createElement('li');
+listItem.textContent = 'Total: ' + Math.round(capHill.totalSales) + ' cookies';
+capHillID.appendChild(listItem);
+
+console.log(capHill.totalSales);
+
+// // //
+// // // //
+// // // //
+// // //
+
+// Alki Cookies Projection
+var alki = {
+  minCustAlki: 2,
+  maxCustAlki: 16,
+  avgCookiesPerCustAlki: 4.6,
+  hourlyCustomerAlki: [],
+  hourlySalesAlki: []
+};
+// method to generate random number of customers per hour and store number of customers per hour in hourlyCustomerAlki
+var randomCustAlki = function() {
+  return (Math.floor(Math.random() * (alki.maxCustAlki - alki.minCustAlki + 1)) + alki.minCustAlki);
+};
+
+var custThatHourAlki = function() {
+  for(var i = 0; i < hours.length; i++) {
+    alki.hourlyCustomerAlki[i] = randomCustAlki();
+  }
+  console.log(alki.hourlyCustomerAlki);
+};
+
+custThatHourAlki();
+
+// Another option to fill hourlyCustomerAlki is by using push
+    //alki.hourlyCustomerAlki.push(randomCustAlki());
+
+// Calculate and store number of cookies purchased for a single hour and storer number of cookies sold per hour in hourlySalesAlki
+var cookiesSoldThatHourAlki = function () {
+  for(var i = 0; i < hours.length; i++) {
+    alki.hourlySalesAlki.push(alki.avgCookiesPerCustAlki * alki.hourlyCustomerAlki[i]);
+  }
+  console.log(alki.hourlySalesAlki);
+};
+
+cookiesSoldThatHourAlki();
+
+// Display the values of each Pike array as unordered lists in the browser
+alki.listSalesAlki = function() {
+  var alkiID = document.getElementById('alkiID');
+  for (var i = 0; i < hours.length; i++) {
+    console.log(alki.hourlySalesAlki[i]);
+    var listItem = document.createElement('li');
+    listItem.textContent = hours[i] + ': ' + Math.round(alki.hourlySalesAlki[i]) + ' cookies';
+    alkiID.appendChild(listItem);
+  }
+};
+
+alki.listSalesAlki();
+
+alki.totalSales = 0;
+
+for (var i = 0; i < hours.length; i++) {
+  alki.totalSales += alki.hourlySalesAlki[i];
+};
+
+var listItem = document.createElement('li');
+listItem.textContent = 'Total: ' + Math.round(alki.totalSales) + ' cookies';
+alkiID.appendChild(listItem);
+
+console.log(alki.totalSales);
