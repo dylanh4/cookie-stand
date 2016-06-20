ffcd4b47529efcad307809d4ea931dfa265e7d77
Dylan Holmes <dylanbholmes@gmail.com>
2016-06-20 15:07:09 -0700

    successfully generate random number of customers per hour for firstAndPike

--- app.js
+++ app.js
@@ -12,39 +12,44 @@
 // General  - Cookie Projections
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm','6pm', '7pm', '8pm'];
 
-var patLocationName = {
+var firstAndPike = {
 // Standard info given for each location
-  minCustomers: minCustomers#,
-  maxCustomers: maxCustomers#,
-  avgCookiesPerCust: avgCookiesPerCust#,
-  hourlyCustomers: ['an','array','listing','hourlyCustomers','from','6am','to','8pm'],
-  hourlySales: ['an','array','listing','hourlySales','from','6am','to','8pm'],
-}
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesPerCust: 6.3,
+  hourlyCustomers: [],
+  hourlySales: []
+};
 // method to generate random number of customers per hour
+var randomCust = function() {
+  return (Math.floor(Math.random() * (firstAndPike.maxCust - firstAndPike.minCust + 1)) + firstAndPike.minCust);
+};
 
-// patLocationName.hourlyCustomers.push(anElement) = function() {
-//   for(var j = 0; j < patLocationName.hours.length; j++) {
-    patLocationName.generateRandom = function () {
-        var hourlyCustomers =  
-
-        getRandomIntInclusive(minCustomers, maxCustomers) {
-        console.log(Math.floor(Math.random() * (maxCustomers - minCustomers + 1)) + minCustomers);
-      }
-    }
+var custThatHour = function() {
+  for(var i = 0; i < hours.length; i++) {
+    firstAndPike.hourlyCustomers[i] = randomCust();
   }
-}
+  console.log(firstAndPike.hourlyCustomers);
+};
+
+custThatHour();
+
+// Another option to fill hourlyCustomers is by using push
+    //firstAndPike.hourlyCustomers.push(randomCust());
+
 // Calculate and store number of cookies purchased for a single hour
 var cookiesSold = function () {
-  return this.generateRandom(maxCustomers, minCustomers) * this.avgCookiesPerCust;
-}
+  return custInHr * avgCookiesPerCust;
+  console.log(custInHr * avgCookiesPerCust);
+};
+
+//
 // Store the results for each location in a separate array... perhaps as a property of the object representing that location
-patLocationName.cookiesSold = function() {
-  for(var i = 0; i < patLocationName.hours.length; i++) {
+firstAndPike.cookiesSold = function() {
+  for(var i = 0; i < firstAndPike.hours.length; i++) {
     console.log(hours[i]);
   }
-}
+};
 
-
-// function getRandomIntInclusive(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
+// firstAndPike.hourlyCustomers.push(anElement) = function() {
+//   for(var j = 0; j < firstAndPike.hours.length; j++) {
